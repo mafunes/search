c42eabfdd7363ecde2e1e97af286ea0c41d171c9
Alejandro <alejandro.funes@mercadolibre.com>
2015-12-08 18:01:08 -0300

    add saving data

--- app/src/main/java/search/mafunes/com/search/MainActivity.java
+++ app/src/main/java/search/mafunes/com/search/MainActivity.java
@@ -1,10 +1,11 @@
 package search.mafunes.com.search;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +13,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +46,22 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        final EditText editText = ((EditText) findViewById(R.id.editText));
+        final SharedPreferences sharedPref = this.getSharedPreferences(
+                getString(R.string.search_file_key), Context.MODE_PRIVATE);
+        editText.setText(sharedPref.getString("query", ""));
+
+        Button button = ((Button) findViewById(R.id.button));
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("query", editText.getText().toString());
+                editor.apply();
+            }
+        });
+
     }
 
     @Override
